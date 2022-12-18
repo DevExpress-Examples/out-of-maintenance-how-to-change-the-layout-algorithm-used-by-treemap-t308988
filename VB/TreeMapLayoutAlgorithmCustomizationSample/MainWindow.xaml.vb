93e391d818d5184565419df2e1d0323e3dab6955
DevExpressExampleBot <fakeEmail>
2022-12-18 15:54:36 +0400

    Source auto update [skip ci]

--- VB/TreeMapLayoutAlgorithmCustomizationSample/MainWindow.xaml.vb
+++ VB/TreeMapLayoutAlgorithmCustomizationSample/MainWindow.xaml.vb
@@ -1,26 +1,30 @@
-﻿Imports DevExpress.Xpf.TreeMap
-Imports System
+Imports DevExpress.Xpf.TreeMap
 Imports System.Windows
 Imports System.Windows.Markup
 
 Namespace TreeMapLayoutAlgorithmCustomizationSample
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
 
-	<ContentProperty("LayoutAlgorithm")>
-	Friend Class LayoutAlgorithmWarapper
-		Public Property LayoutAlgorithm() As TreeMapLayoutAlgorithmBase
-		Public Property LayoutAlgorithmName() As String
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-	<ContentProperty("Direction")>
-	Friend Class LayoutDirectionWrapper
-		Public Property Direction() As LayoutDirection
-		Public Property DirectionName() As String
-	End Class
+    <ContentProperty("LayoutAlgorithm")>
+    Friend Class LayoutAlgorithmWarapper
+
+        Public Property LayoutAlgorithm As TreeMapLayoutAlgorithmBase
+
+        Public Property LayoutAlgorithmName As String
+    End Class
+
+    <ContentProperty("Direction")>
+    Friend Class LayoutDirectionWrapper
+
+        Public Property Direction As LayoutDirection
+
+        Public Property DirectionName As String
+    End Class
 End Namespace
